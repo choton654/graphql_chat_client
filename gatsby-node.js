80db3b686644d1f9100185074066ad4fd8b3a997
Choton <choton654@gmail.com>
2020-11-15 17:20:31 +0530

    'add-gatsby-config'

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,32 +1,10 @@
-const path = require("path")
+// const path = require("path")
 
-exports.onCreatePage = async ({ page, actions }) => {
-  const { createPage } = actions
+// exports.onCreatePage = async ({ page, actions }) => {
+//   const { createPage } = actions
 
-  if (page.path.match(/^\/app/)) {
-    page.matchPath = "/app/*"
-    createPage(page)
-  }
-}
-// exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-//   if (stage === "build-html") {
-//     actions.setWebpackConfig({
-//       module: {
-//         rules: [
-//           {
-//             test: /bad-module/,
-//             use: loaders.null(),
-//           },
-//         ],
-//       },
-//     })
+//   if (page.path.match(/^\/app/)) {
+//     page.matchPath = "/app/*"
+//     createPage(page)
 //   }
-// }
-
-// exports.onCreateWebpackConfig = ({ actions }) => {
-//   actions.setWebpackConfig({
-//     resolve: {
-//       modules: [path.resolve(__dirname, "src"), "node_modules"],
-//     },
-//   })
 // }
